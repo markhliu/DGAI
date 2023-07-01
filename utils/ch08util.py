69ff12e7193510c17b259bc7d568022a9f54727d
Mark Liu <50116107+markhliu@users.noreply.github.com>
2023-07-01 09:52:02 -0400

    Add files via upload

---
+++ utils/ch08util.py
@@ -0,0 +1,219 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import torch
+import numpy as np
+import torch.nn as nn
+from PIL import Image
+import os
+from torch.utils.data import Dataset
+
+
+
+from tqdm import tqdm
+from torchvision.utils import save_image
+
+def test(i,A,B,fake_A,fake_B):
+    save_image(A*0.5+0.5,f"files/ch08/A{i}.png")
+    save_image(B*0.5+0.5,f"files/ch08/B{i}.png")
+    save_image(fake_A*0.5+0.5,f"files/ch08/fakeA{i}.png")
+    save_image(fake_B*0.5+0.5,f"files/ch08/fakeB{i}.png")  
+
+def train_epoch(disc_A, disc_B, gen_A, gen_B, loader, opt_disc,
+        opt_gen, l1, mse, d_scaler, g_scaler,cycle_lambda,device):
+    loop = tqdm(loader, leave=True)
+
+    for i, (A,B) in enumerate(loop):
+        A=A.to(device)
+        B=B.to(device)
+
+        # Train Discriminators A and B
+        with torch.cuda.amp.autocast():
+            fake_A = gen_A(B)
+            D_A_real = disc_A(A)
+            D_A_fake = disc_A(fake_A.detach())
+            D_A_real_loss = mse(D_A_real, torch.ones_like(D_A_real))
+            D_A_fake_loss = mse(D_A_fake, torch.zeros_like(D_A_fake))
+            D_A_loss = D_A_real_loss + D_A_fake_loss
+
+            fake_B = gen_B(A)
+            D_B_real = disc_B(B)
+            D_B_fake = disc_B(fake_B.detach())
+            D_B_real_loss = mse(D_B_real, torch.ones_like(D_B_real))
+            D_B_fake_loss = mse(D_B_fake, torch.zeros_like(D_B_fake))
+            D_B_loss = D_B_real_loss + D_B_fake_loss
+
+            # Average loss of the two discriminators
+            D_loss = (D_A_loss + D_B_loss) / 2
+
+        opt_disc.zero_grad()
+        d_scaler.scale(D_loss).backward()
+        d_scaler.step(opt_disc)
+        d_scaler.update()
+        # Train the two generators 
+        with torch.cuda.amp.autocast():
+            D_A_fake = disc_A(fake_A)
+            D_B_fake = disc_B(fake_B)
+            loss_G_A = mse(D_A_fake, torch.ones_like(D_A_fake))
+            loss_G_B = mse(D_B_fake, torch.ones_like(D_B_fake))
+            # NEW in Cycle GANs: cycle loss
+            cycle_B = gen_B(fake_A)
+            cycle_A = gen_A(fake_B)
+            cycle_B_loss = l1(B, cycle_B)
+            cycle_A_loss = l1(A, cycle_A)
+            # Total generator loss
+            G_loss=(loss_G_A+loss_G_B+cycle_A_loss*cycle_lambda
+                +cycle_B_loss*cycle_lambda)
+        opt_gen.zero_grad()
+        g_scaler.scale(G_loss).backward()
+        g_scaler.step(opt_gen)
+        g_scaler.update()
+        if i % 100 == 0:
+            test(i,A,B,fake_A,fake_B)            
+        loop.set_postfix(D_loss=D_loss.item(),G_loss=G_loss.item())
+
+
+
+
+
+
+
+
+
+
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, 
+                 down=True, use_act=True, **kwargs):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 
+                      padding_mode="reflect", **kwargs)
+            if down
+            else nn.ConvTranspose2d(in_channels, 
+                                    out_channels, **kwargs),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(inplace=True) if use_act else nn.Identity())
+    def forward(self, x):
+        return self.conv(x)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            ConvBlock(channels,channels,kernel_size=3,padding=1),
+            ConvBlock(channels,channels,
+                      use_act=False, kernel_size=3, padding=1))
+    def forward(self, x):
+        return x + self.block(x)
+
+class Generator(nn.Module):
+    def __init__(self, img_channels, num_features=64,
+                 num_residuals=9):
+        super().__init__()
+        self.initial = nn.Sequential(
+            nn.Conv2d(img_channels,num_features,kernel_size=7,
+                stride=1,padding=3,padding_mode="reflect",),
+            nn.InstanceNorm2d(num_features),
+            nn.ReLU(inplace=True))
+        self.down_blocks = nn.ModuleList(
+            [ConvBlock(num_features,num_features*2,kernel_size=3,
+                       stride=2, padding=1),
+            ConvBlock(num_features*2,num_features*4,kernel_size=3,
+                stride=2,padding=1)])
+        self.res_blocks = nn.Sequential(
+            *[ResidualBlock(num_features * 4) 
+            for _ in range(num_residuals)])
+        self.up_blocks = nn.ModuleList(
+            [ConvBlock(num_features * 4, num_features * 2,
+                    down=False, kernel_size=3, stride=2,
+                    padding=1, output_padding=1),
+                ConvBlock(num_features * 2, num_features * 1,
+                    down=False,kernel_size=3, stride=2,
+                    padding=1, output_padding=1)])
+        self.last = nn.Conv2d(num_features * 1, img_channels,
+            kernel_size=7, stride=1,
+            padding=3, padding_mode="reflect")
+    def forward(self, x):
+        x = self.initial(x)
+        for layer in self.down_blocks:
+            x = layer(x)
+        x = self.res_blocks(x)
+        for layer in self.up_blocks:
+            x = layer(x)
+        return torch.tanh(self.last(x))
+
+class Block(nn.Module):
+    def __init__(self,in_channels,out_channels,stride):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels,out_channels,4,stride,1,
+                bias=True,padding_mode="reflect"),
+            nn.InstanceNorm2d(out_channels),
+            nn.LeakyReLU(0.2,inplace=True))
+    def forward(self, x):
+        return self.conv(x)
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, features=[64,128,256,512]):
+        super().__init__()
+        self.initial = nn.Sequential(
+            nn.Conv2d(in_channels,features[0],
+                kernel_size=4,stride=2,padding=1,
+                padding_mode="reflect"),
+            nn.LeakyReLU(0.2, inplace=True))
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(Block(in_channels, feature, 
+                stride=1 if feature == features[-1] else 2))
+            in_channels = feature
+        layers.append(nn.Conv2d(in_channels,1,kernel_size=4,
+                stride=1,padding=1,padding_mode="reflect"))
+        self.model = nn.Sequential(*layers)
+    def forward(self, x):
+        out = self.model(self.initial(x))
+        return torch.sigmoid(out)
+
+class LoadData(Dataset):
+    def __init__(self, root_A, root_B, transform=None):
+        super().__init__()
+        self.root_A = root_A
+        self.root_B = root_B
+        self.transform = transform
+        self.A_images = os.listdir(root_A)
+        self.B_images = os.listdir(root_B)
+        self.len_data = max(len(self.A_images),
+                            len(self.B_images))
+        self.A_len = len(self.A_images)
+        self.B_len = len(self.B_images)
+    def __len__(self):
+        return self.len_data
+    def __getitem__(self, index):
+        A_img = self.A_images[index % self.A_len]
+        B_img = self.B_images[index % self.B_len]
+        A_path = os.path.join(self.root_A, A_img)
+        B_path = os.path.join(self.root_B, B_img)
+        A_img = np.array(Image.open(A_path).convert("RGB"))
+        B_img = np.array(Image.open(B_path).convert("RGB"))
+        if self.transform:
+            augmentations = self.transform(image=B_img,
+                                           image0=A_img)
+            B_img = augmentations["image"]
+            A_img = augmentations["image0"]
+        return A_img, B_img 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
